53e4c53fcbc424030222216c1225b89982839798
code-vedant <162135022+code-vedant@users.noreply.github.com>
2026-09-12 19:33:51 +0530

    add initial template for competitive programming solution

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+void solve() {
+    // Your solution
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
